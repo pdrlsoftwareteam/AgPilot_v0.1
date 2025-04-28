07ee3c7dcc9f3d472feadb6c6c323ba7e2109e9d
ManishSakharwade <manish.sakharwade@pdrl.in>
2025-04-28 11:43:19 +0530

    Generic-CAN-Driver-Support

--- libraries/AP_RangeFinder/AP_RangeFinder_USD1_CAN.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_USD1_CAN.cpp
@@ -1,19 +1,46 @@
 #include "AP_RangeFinder_USD1_CAN.h"
 
-#if AP_RANGEFINDER_USD1_CAN_ENABLED
+//#if AP_RANGEFINDER_USD1_CAN_ENABLED
 
 #include <AP_HAL/AP_HAL.h>
-
+#include <GCS_MAVLink/GCS.h>
 /*
   constructor
  */
-AP_RangeFinder_USD1_CAN::AP_RangeFinder_USD1_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params, uint8_t mlowerByte, uint8_t mupperByte, uint8_t msensId) :
+AP_RangeFinder_USD1_CAN::AP_RangeFinder_USD1_CAN(RangeFinder::RangeFinder_State &_state, AP_RangeFinder_Params &_params, uint32_t msensId) :
     AP_RangeFinder_Backend(_state, _params)
 {
-	_lowerByte = mlowerByte;
-	_upperByte = mupperByte;
-	_sensId = msensId;
+	_distance_count = 0;
+	_distance_sum = 0;
+	_msg_byte = _params.msgByte;
+    _sensId = _params.msgId & 0x7ff;
+    if(msensId == 0x7FF)
+    {
+    	_sensId = 0x7FF;
+    }
+    if(msensId == 0x00)
+    {
+    	_sensId = 0x00;
+    	_msg_byte = 0x00000021;
+    }
 	_msem = &_sem;
+
+	for(uint8_t i=8;i>0;i--)
+	{
+		uint8_t nibble =  _msg_byte>> ((i-1)*4) & 0x0F;
+		if(nibble != 0)
+		{
+			if(nibble == 1)
+				_arr[0] = i;
+			else if(nibble == 2)
+				_arr[1] = i;
+			else if(nibble == 3)
+				_arr[2] = i;
+			else if(nibble == 4)
+				_arr[3] = i;
+		}
+	}
+
 	AP_CANDataDistribuer::getInstance()->addCANDataListener(this);
 }
 
@@ -37,7 +64,7 @@ void AP_RangeFinder_USD1_CAN::update(void)
 //CAN data distributer
 AP_CANDataDistribuer *AP_CANDataDistribuer::instance = nullptr;
 AP_CANDataDistribuer::AP_CANDataDistribuer():
-CANSensor("USD1")
+				CANSensor("USD1")
 {
     register_driver(AP_CANManager::Driver_Type_USD1);
 }
@@ -47,15 +74,35 @@ void AP_CANDataDistribuer::handle_frame(AP_HAL::CANFrame &frame)
 {
     for(int i = 0; i < totalDeviceHandled;i++)
 	{
-    	if(rngfndInst[i]->_sensId == frame.id)
+    	uint32_t can_id = frame.id & 0x7ff;
+    	if(rngfndInst[i]->_sensId == 0x7FF)
+		{
+    		if((AP_HAL::millis() - now_us) > 250)
+    		{
+    	     	now_us = AP_HAL::millis();
+				gcs().send_text(MAV_SEVERITY_INFO,"Id: %lu b[0]:%d b[1]:%d b[2]:%d b[3]:%d b[4]:%d b[5]:%d b[6]:%d b[7]:%d",
+															frame.id,frame.data[0],frame.data[1],frame.data[2],frame.data[3],frame.data[4],frame.data[5],frame.data[6],frame.data[7]);
+				WITH_SEMAPHORE(rngfndInst[i]->_msem);
+				return;
+    		}
+		}
+    	else if(rngfndInst[i]->_sensId == can_id || rngfndInst[i]->_sensId == 0x00)
     	{
             WITH_SEMAPHORE(rngfndInst[i]->_msem);
-    		const uint16_t dist_cm = (frame.data[rngfndInst[i]->_lowerByte]<<8) | frame.data[rngfndInst[i]->_upperByte];
-    		rngfndInst[i]->_distance_sum += dist_cm * 0.01;
+            uint32_t dist_cm = 0;
+            uint8_t count = 0;
+            for (int j = 4; j > 0; j--) {
+                if (rngfndInst[i]->_arr[j-1]) {
+                    dist_cm |= frame.data[rngfndInst[i]->_arr[j-1]-1] << (count * 8);
+                    count++;
+                }
+            }
+            rngfndInst[i]->_distance_sum += dist_cm * 0.01;
     		rngfndInst[i]->_distance_count = rngfndInst[i]->_distance_count + 1;
     		return;
     	}
+
 	}
 }
 
-#endif  // AP_RANGEFINDER_USD1_CAN_ENABLED
+//#endif  // AP_RANGEFINDER_USD1_CAN_ENABLED
